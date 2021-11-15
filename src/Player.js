07fda051a9a7b2a0b4f7e87a0bb233b401ec4e3a
Alex Smetukh <alex.smetukh@gmail.com>
2021-11-15 13:05:06 +0200

    update button 3d styles, add Unique internal code match, get models list, pass assetId, save 3d svg, add constants

--- src/Player.js
+++ src/Player.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
 
-const Player = ({ setShowModal }) => {
+const Player = ({ setShowModal, assetId }) => {
 
   useEffect(() => {
     (async () => {
       try {
         window
         .threekitPlayer({
+          assetId,
           authToken: "31bae9f8-18d7-4e8a-8b28-bdd680d0ecd0",
           el: document.getElementById("player"),
           stageId: undefined,
-          assetId: window.assetId,
           initialConfiguration: {
             //  'Attribute Name': {
             //     assetId: '4a9f7980-78a4-4ace-8cda-f2799936c4dc',
@@ -32,12 +32,21 @@ const Player = ({ setShowModal }) => {
       }
     })();
 
-  }, [])
+  }, [assetId])
   return (
     <div className="modal">
       <div className="modal-wrapper">
         <div className="modal-content">
-          <div className='closeButton' onClick={() => { setShowModal(false) }}>X</div>
+          {/* <div className='closeButton' onClick={() => { setShowModal(false) }}>X</div> */}
+          <div className="closeButton"
+          onClick={() => setShowModal(false)}
+        >
+          <img
+            // src="https://preview.threekit.com/js/4169b49e18e94d509b08ad80920c104a.svg"
+            src={require('./assets/SVG/close.svg').default}
+            alt="icon" />
+          {/* <div className="arButtonText">View in 3D</div> */}
+          </div>
           <div id="player"></div>
         </div>
 
