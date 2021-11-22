169dcc09e17fbdc8408b3719bb4e0dc89de0ccb5
Alex Smetukh <alex.smetukh@gmail.com>
2021-11-22 11:32:59 +0200

    remove script from html

--- src/components/ThreekitButton/ThreekitButton.js
+++ src/components/ThreekitButton/ThreekitButton.js
@@ -1,12 +1,9 @@
-import './ThreekitButton.css';
+import styles from './ThreekitButton.module.css';
 
-const ThreekitButton = ({ setShowModal, assetId }) => {
-  return (
-    <button className="viewButton" onClick={() => setShowModal(true)} disabled={!assetId}>
-      <img src={require('../../assets/SVG/threekit3d.svg').default} alt="icon" />
-      <div className="arButtonText" title={!!assetId ? undefined : 'Not available'}>View in 3D</div>
-    </button>
-  )
-}
+const ThreekitButton = ({ setShowModal, assetId }) =>
+  <button className={styles.viewButton} onClick={() => setShowModal(true)} disabled={!assetId}>
+    <img src={require('../../assets/SVG/threekit3d.svg').default} alt="icon" />
+    <div className={styles.arButtonText} title={!!assetId ? undefined : 'Not available'}>View in 3D</div>
+  </button>;
 
 export default ThreekitButton;
